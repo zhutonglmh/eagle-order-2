3170960c390aa58f0e53ae439971d3e59e36e688
zhutong <zhutonglmh@163.com>
2017-12-20 11:30:56 +0800

    测试git使用

---
+++ src/main/java/com/example/demo/Demo2.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.order.pojo.Food;
+
+@RestController
+public class Demo2 {
+    
+    @Resource
+    private Food food;
+    
+    @RequestMapping("/boot")
+    public Map<String,Object> demo2(){
+        Map<String,Object> map = new HashMap<String,Object>();
+        food.setId(4);
+        map.put("朱同", "帅哥！！");
+        return map;
+    }
+}
